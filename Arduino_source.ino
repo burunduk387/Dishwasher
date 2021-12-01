e6398647fe07ee59ad371661d656001a92ce5f00
Mark Blumenau <burunduk10@inbox.ru>
2021-12-01 23:46:55 +0300

    Cleaning

--- Arduino_source.ino
+++ Arduino_source.ino
@@ -1,3 +1,4 @@
+
 #define NOTE_E5  659
 
 int noise[] =
@@ -5,18 +6,19 @@ int noise[] =
   NOTE_E5, NOTE_E5, NOTE_E5,
   NOTE_E5, NOTE_E5, NOTE_E5
 };
+
 int noise_time[] = 
 {
   8, 8, 4,
   8, 8, 4
 };
+
 int flag = 0;
 const int buzzer = 9; 
-const int on_off = 7; 
-const int thirty_sec = 8; 
 const int mosfet = 10;
 const int pressure = A7;
 const int water = A6;
+
 void buzz() 
 {
   int len = sizeof(noise_time) / sizeof(int);
@@ -28,17 +30,17 @@ void buzz()
     noTone(buzzer);
   }
 }
+
 void setup() 
 {
   pinMode(mosfet, OUTPUT);
-  pinMode(on_off, INPUT);
-  pinMode(thirty_sec, INPUT);
-  pinMode(mosfet, OUTPUT);
   digitalWrite(mosfet, LOW);
 }
+
 void loop() 
 {
-  if ((analogRead(water) <512) and (flag == 0))
+  // if the pressure is too low and no water in tank - don't pump and make noise
+  if ((analogRead(water) < 512) and (flag == 0))
   {
     flag = 1;
     buzz();
@@ -47,10 +49,13 @@ void loop()
     delay(3000);
     buzz();
   }
+
    if (analogRead(water) >= 512)
   {
     flag = 0;
   }
+
+  // With water in tank and low pressure - turn the pump on
   if ((analogRead(pressure) < 120) and (analogRead(water) >= 512))
   {
     digitalWrite(mosfet, HIGH);
@@ -60,29 +65,5 @@ void loop()
     }
     digitalWrite(mosfet, LOW);
   }
- /* if ((digitalRead(on_off) == HIGH) and (analogRead(water) >= 512))
-  {
-    digitalWrite(mosfet, HIGH);
-    while (analogRead(water) >= 512)
-    {
-      delay(50);
-      if (digitalRead(on_off) == HIGH)
-      {
-        break;
-      }
-    }
-    digitalWrite(mosfet, LOW);
-  }
-  if ((digitalRead(thirty_sec) == HIGH) and (analogRead(water) >= 512))
-  {
-    digitalWrite(mosfet, HIGH);
-    int i = 0;
-    while ((analogRead(water) >= 512) and (i < 30000))
-    {
-      delay(50);
-      i += 50;
-    }
-    digitalWrite(mosfet, LOW);
-  }*/
   delay(50);
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Dishwasher
-My code for arduino controlling a DC pump for a dishwasher.
+My code for Arduino Nano controlling a DC pump for a dishwasher. Basically it turns the pump on/off depending on the pressure sensor value. On low water stops pumping (in order no to damage the pump)
+and makes some noise.
